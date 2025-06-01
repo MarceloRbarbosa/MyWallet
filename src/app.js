b3f0784fed8609bb2eaa771349382c09d647796e
Marcelo Ribeiro Barbosa <marceloribeirobarbosa92@gmail.com>
2025-05-31 23:10:53 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express, {json} from "express";
+import chalk from "chalk";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(chalk.bgGreen(`Servidor rodando na porta ${PORT}`))
+});
