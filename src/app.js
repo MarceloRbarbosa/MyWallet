24ae32b553eafcc914ee4f50e0150916aa19beba
Marcelo Ribeiro Barbosa <marceloribeirobarbosa92@gmail.com>
2025-06-02 13:07:59 -0300

    fix database e add schemas

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
-import express, {json} from "express";
+import "./config/database.js";
+import express, { json } from "express";
 import chalk from "chalk";
-
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 app.use(json());
+app.use(cors());
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, ()=>{
-    console.log(chalk.bgGreen(`Servidor rodando na porta ${PORT}`))
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(chalk.blue(`Servidor está rodando na porta`) + chalk.magenta(` ${port}`))
 });
--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,16 @@
-import chalk from "chalk"
 import { MongoClient } from "mongodb";
+import chalk from "chalk";
 
 import dotenv from "dotenv"
-dotenv.config()
+dotenv.config();
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 try {
     await mongoClient.connect();
-    console.log(chalk.green("MongoDB conectado!"))
-} catch (error) {
-    console.log(error.message);
+    console.log(chalk.bgGreen(`MongoDB conectado!`));
+} catch (err) {
+    console.log(chalk.bgRed(`Erro ao conectar ao banco de dados!`), err.message);
 }
 
 export const db = mongoClient.db();
